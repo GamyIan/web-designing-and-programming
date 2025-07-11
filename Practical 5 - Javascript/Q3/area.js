365a735613be8430fbf4875ca1ca3902f80e517b
Ian Reddy <73981521+GamyIan@users.noreply.github.com>
2025-07-11 15:43:41 +0000

    Used let in practical 5

--- Practical 5 - Javascript/Q3/area.js
+++ Practical 5 - Javascript/Q3/area.js
@@ -1,5 +1,5 @@
 document.write("<h2>Area of Circle Calculator</h2>");
-var r = 10;
+let r = 10;
 var area = r * r * Math.PI;
 document.write("Radius: " + r + "<br>");
 document.write("Area of Circle: " + area);
