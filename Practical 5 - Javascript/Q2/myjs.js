173915e88b42d4d476c49e8530a320a80ef56a31
Ian Reddy <73981521+GamyIan@users.noreply.github.com>
2025-07-11 04:30:06 +0000

    Added Practical 4 and 5

---
+++ Practical 5 - Javascript/Q2/myjs.js
@@ -0,0 +1,7 @@
+document.write("<h2>Student Details:</h2>");
+document.write("<p>Name: Ian</p>");
+document.write("<p>UID: 2405024</p>");
+document.write("<p>Course: BSc.IT</p>");
+document.write("<p>Course Code: 69420</p>");
+document.write("<p>DOB: 25/10/2005</p>");
+document.write("<p>Age: 19</p>");
