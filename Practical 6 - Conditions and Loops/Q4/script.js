c069869b6829e291a3724ad4660e3199793bb004
Ian Reddy <73981521+GamyIan@users.noreply.github.com>
2025-07-18 15:54:20 +0000

    Added Practical 6 Q1-Q6

---
+++ Practical 6 - Conditions and Loops/Q4/script.js
@@ -0,0 +1,13 @@
+/*
+Q4. A Cinema charges different ticket prices based on age
+Under 5 --> Free
+5 - 12  --> 100
+13 - 59 --> 150
+60+     --> 80
+
+Perform the following using javascript:
+1. Ask the user for their age using prompt box
+2. Use if, else if, else to determine the ticket price
+3. Show the result using the alert box
+
+*/
